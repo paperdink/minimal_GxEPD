242d77e92c5c2e5dcc8cf375468c7fd4d4f6fcc5
Rohit Gujarathi <gujju.rohit@gmail.com>
2021-11-14 22:19:30 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Minimal example using GxEPD
+
+![Full Image](minimal.jpg)
+
+## Install Library Dependencies (from arduino library manager)
+  - Adafruit GFX library by Adafruit (tested with v1.7.5)
+  - GxEPD by Jean-Marc Zingg version (tested with v3.0.9)
+
+## Usage
+Upload sketch and you should see Albert Einstein
---
+++ minimal_GxEPD.ino
@@ -0,0 +1,36 @@
+
+#include <GxEPD.h>
+#include <GxGDEW042T2/GxGDEW042T2.h>      // 4.2" b/w
+#include <GxIO/GxIO_SPI/GxIO_SPI.h>
+#include <GxIO/GxIO.h>
+#include "alberteinstein.h"
+
+GxIO_Class io(SPI, /*CS=*/ 22, /*DC=*/ 15, /*RST=*/ 13);
+GxEPD_Class display(io, /*RST=*/ 13, /*BUSY=*/ 34);
+
+void setup()
+{
+  pinMode(12, OUTPUT);
+  
+  // Enable power to EPD
+  digitalWrite(12, LOW);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("setup");
+
+  display.init(115200); // enable diagnostic output on Serial
+  display.setRotation(0);
+  Serial.println("Setup done");
+
+  display.fillScreen(GxEPD_WHITE);
+  display.drawBitmap(gImage_alberteinstein, 0, 0, 400, 300, GxEPD_BLACK);
+  display.update();
+
+  // Disable power to EPD
+  digitalWrite(12, LOW);
+}
+
+void loop()
+{
+  // Nothing here  
+}
